cabc4346e3513af9a323882ddcedd9ea942c35f5
Srinivasa Challa <Srinivasa.Challa@cyberark.com>
2022-03-25 00:09:09 -0700

    CC-102938 SCA - Identity Implementation - CLI with Hook API Flow
    Root cause:
      Update user agent to custom value to identify that request is coming from AWS CLI.
    
    Reason for regression:
       "N/A"
    
    Resolution:
        Implemented code to update header User-Agent with custom name
    
    GUI Changes:
       "N/A"
    
    Security Review:
        # N/A
    
    Manual Code Validation (Step Through):
    
    New Unit Tests:
         "N/A"
    
    Unit Test Results:
    	"N/A"
    
    Jira URL:
        https://ca-il-jira.il.cyber-ark.com:8443/browse/CC-102938
    
    Steps to reproduce:
       "See above Jira"
    
    Notes to QA:
         "N/A"
    
    Smoketest Results:
       "N/A"
    
    Files affected:
      restclient.py file has modified

--- AWS CLI - Idaptive V1/core/restclient.py
+++ AWS CLI - Idaptive V1/core/restclient.py
@@ -26,6 +26,11 @@ def call_rest_post(endpoint, method, body, headers, certpath, proxy, debug):
     if 'cache-control' not in headers:
         headers['cache-control'] = "no-cache"
 
+    headers.update(
+        {
+            'User-Agent': 'CyberArkIdentity-AWS-Cli'
+        })
+
     logging.info("Calling " + endpoint + " with headers : " + str(headers))
     if (debug):
         logging.info("Request : " + str(body))
